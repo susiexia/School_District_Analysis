b0d2a2cb27f4228b3bff5a79afa7600402eed0f1
susiexia <57109686+susiexia@users.noreply.github.com>
2019-12-12 23:25:57 -0800

    format code and make a copy

--- Draft_PyCitySchools_python.py
+++ Draft_PyCitySchools_python.py
@@ -8,6 +8,9 @@ try:
 	print(os.getcwd())
 except:
 	pass
+# %%
+#use os.path.join(), indirectly load file
+#use Pandas.read_csv to read (without open files) into DataFrame
 
 
 # %%
@@ -23,27 +26,66 @@ student_data_df = pd.read_csv(student_data_to_load)
 
 # %%
 
+student_data_df.tail(10)
+student_data_df.describe()
 student_data_df.head()
 
 # %%
 school_data_df.head()
+# %%
+student_data_df.count()
 
-# %% [markdown]
-# ##Clean student_names, remove inappropriate prefixes and suffixes
 
 # %%
+school_data_df.isnull()
+
+
+# %%
+student_data_df.isnull().sum()
+
+
+# %%
+student_data_df.notnull().sum()
+
+
+# %%
+#use _df.dropna() & _df.fillna(0) to deal with missing data
+
+
+# %%
+#check data types, !! Attribute
+school_data_df.dtypes
+
+
+# %%
+student_data_df.dtypes
+
+
+# %%
+student_data_df.grade.dtype
+student_data_df['reading_score'].dtype
+
+
+# %%
+# clean incorrect student names
 prefixes_suffixes = ["Dr. ", "Mr. ","Ms. ", "Mrs. ", "Miss ", " MD", " DDS", " DVM", " PhD"]
 
 for word in prefixes_suffixes:
     student_data_df['student_name'] = student_data_df['student_name'].str.replace(word,'')
-student_data_df.head(10)
+print(student_data_df.head(10))
 
 # %% [markdown]
-# #TABLE No.1: A high level snapshot of district's key metircs
-# ## district_summary_df
+# ### merge two dataFrame
+# 
 
 # %%
-#Merge school_data_df(right) and student_data_df(left) on a shared column 'school_name'
+#check two DataFrame columns
+print(student_data_df.columns)
+print(len(school_data_df.columns))
+
+
+# %%
+#merge school_data_df(right) and student_data_df(left) on a shared column 'school_name'
 
 school_data_complete_df = pd.merge(student_data_df,school_data_df, on=['school_name'])
 school_data_complete_df.head()
@@ -71,7 +113,7 @@ overall_passing_percentage = (passing_math_percentage + passing_reading_percenta
 print(f'Overall pass Rate:{overall_passing_percentage:.2f}%')
 
 # %% [markdown]
-# ### a summary new Dataframe to collect all key metrics generated 
+# ### a summary new Dataframe to collect all key metrics aboved
 
 # %%
 district_summary_df = pd.DataFrame([{"Total Schools": school_count,
@@ -99,40 +141,75 @@ district_summary_df["% Overall Passing"] = district_summary_df["% Overall Passin
 district_summary_df
 
 # %% [markdown]
-# #TABLE No.2: overview of key metrics for each school 
-# ## per_school_summary_df
+# ## Table: overview of key metrics for each school |||| school name as index 
 
 # %%
 #retrieve school name as row labels, school type as the first column, to make a new DataFrame
-#use set_index() fuction
 
 per_school_types_Series=school_data_df.set_index(['school_name']).type
 
+
+#or: per_school_summary_df = pd.DataFrame({'school_type':per_school_types_Series})
 per_school_summary_df = pd.DataFrame(per_school_types_Series)
 
+per_school_summary_df
+
+# %% [markdown]
+# #### very useful column method: to get frequency of every value appeared, same results as .set_index() method
+# #### in a descending order
+
 # %%
+
 per_school_student_count_series = school_data_complete_df.school_name.value_counts()
+per_school_student_count_series
 
-per_school_size_Series=school_data_df.set_index(['school_name'])['size']
-
-per_school_budget_Series=school_data_df.set_index(['school_name'])['budget']
-
-per_school_capita_Series = per_school_budget_Series/per_school_size_Series
 
 # %%
-# use groupby() fuction 
+per_school_size_Series=school_data_df.set_index(['school_name'])['size']
+per_school_size_Series
+
+
+# %%
+per_school_budget_Series=school_data_df.set_index(['school_name'])['budget']
+per_school_budget_Series
+
+# %%
+per_school_capita_Series = per_school_budget_Series/per_school_size_Series
+per_school_capita_Series
+
+# %%
+F_per_school_capita_Series = per_school_capita_Series.map('${:.0f}'.format)
+F_per_school_capita_Series
+
+# %% [markdown]
+# #### groupby()
+# %%
 per_school_grp_math = school_data_complete_df.groupby(['school_name']).math_score.agg('mean')
 per_school_grp_reading = school_data_complete_df.groupby(['school_name']).reading_score.mean()
+per_school_grp_math
+per_school_grp_reading
+# %% 
+# groupby two Series 
+per_school_student_grp_df = school_data_complete_df.groupby(by=['school_name','student_name'], axis = 0, as_index = False).mean()
 
+# %%[markdown]
+# #### groupby() and percentage
+# %%
 pass_math_filter_df = school_data_complete_df[school_data_complete_df.math_score >= 70]
 pass_reading_filter_df = school_data_complete_df[school_data_complete_df.reading_score >= 70]
 
 per_school_passingMath_grp_Series = pass_math_filter_df.groupby(['school_name']).count()['student_name']
 per_school_passingReading_grp_Series = pass_reading_filter_df.groupby(['school_name']).count()['student_name']
+per_school_passingReading_grp_Series
 
 per_school_passing_math = per_school_passingMath_grp_Series/per_school_size_Series * 100
 per_school_passing_reading = per_school_passingReading_grp_Series/per_school_size_Series * 100
 per_overall_passing_percentage = (per_school_passing_math + per_school_passing_reading) /2
+per_overall_passing_percentage
+
+# %%[markdown]
+# #### New DataFrame of key metrics for per_school_summary_df
+# ##### This summary dont need to groupby or set_index, because all Columns are index by school_name !!!
 
 # %%
 per_school_summary_df = pd.DataFrame({'Total Students': per_school_size_Series,
@@ -153,19 +230,18 @@ per_school_summary_df = per_school_summary_df[per_school_column_reorder]
 per_school_summary_df.head()                                                                                                                                                                                     
 
 # %% [markdown]
-# #TABLE No.3 Top 5 and bottom 5 performing schools, based on the overall passing rate
-# ##use sort_values()
-# ##top_schools_df and bottom_schools_df
+# ###Find out highest and lowest performance based on overall passing, sort_values()
 # %%
 top_schools_df = per_school_summary_df.sort_values(['% Overall Passing'], ascending = False)
 top_schools_df.head(5)
 bottom_schools_df = per_school_summary_df.sort_values(['% Overall Passing'])
 bottom_schools_df.head()
 # %% [markdown]
-# #TABLE No.4: The average math score received by students in each grade level at each school
-# ## grade_math_summary_df
+# ### grade-level metrics, groupby school_name as index, and grade as columns. the value would like be ave score
 
 # %%
+# use school_data_complete_df dataframe get grade-level information and score series
+school_data_complete_df.head()
 # filter different grade, create DF for each grade level
 complete_9th_filtered_df = school_data_complete_df[(school_data_complete_df.grade == '9th')]
 complete_10th_filtered_df = school_data_complete_df[(school_data_complete_df.grade == '10th')]
@@ -173,7 +249,7 @@ complete_11th_filtered_df = school_data_complete_df[(school_data_complete_df.gra
 complete_12th_filtered_df = school_data_complete_df[(school_data_complete_df.grade == '12th')]
 
 # %%
-
+# groupby and get avg scores
 grade9th_math_school_grp_Series = complete_9th_filtered_df.groupby(['school_name']).mean()['math_score']
 grade10th_math_school_grp_Series = complete_10th_filtered_df.groupby(['school_name']).mean()['math_score']
 grade11th_math_school_grp_Series = complete_11th_filtered_df.groupby(['school_name']).mean()['math_score']
@@ -184,6 +260,7 @@ grade10th_reading_school_grp_Series = complete_10th_filtered_df.groupby(['school
 grade11th_reading_school_grp_Series = complete_11th_filtered_df.groupby(['school_name']).mean()['reading_score']
 grade12th_reading_school_grp_Series = complete_12th_filtered_df.groupby(['school_name']).mean()['reading_score']
 
+#grade9th_reading_school_grp_Series 
 
 # %%
 grade_math_summary_df = pd.DataFrame({'9th':grade9th_math_school_grp_Series.map('{:.1f}'.format),
@@ -193,9 +270,6 @@ grade_math_summary_df = pd.DataFrame({'9th':grade9th_math_school_grp_Series.map(
 # remove index column's name
 grade_math_summary_df.index.name = None
 grade_math_summary_df
-# %% [markdown]
-# #TABLE No.5: The average reading score received by students in each grade level at each school
-# ## grade_reading_summary_df
 # %%
 grade_reading_summary_df = pd.DataFrame({'9th':grade9th_reading_school_grp_Series.map('{:.1f}'.format),
                             '10th':grade10th_reading_school_grp_Series.map('{:.1f}'.format),
@@ -205,17 +279,33 @@ grade_reading_summary_df.index.name = None
 grade_reading_summary_df
 
 # %% [markdown] 
-# #TABLE No.6: School performance based on the budget per student
-# ##use bins and cut() function to get spending_school_summary_df
+# ## School performance based on the budget per student
+# #### spending bins as index
+# #### find how does budget per student affect the school performance
 # %%
+#per_school_capita_Series is Series that the budget per student for each school
+per_school_capita_Series.describe()
+
+# %% 
+# pd.cut() 
+#equally cut! but not group scholls fair, to fairly distribution we manually adjest bins edge
+#cutted_capita_categorical_S = pd.cut(per_school_capita_Series, 4, precision=0) 
+
 spending_bins = [0,585,630,645,675]
 group_names = ["<$584", "$585-629", "$630-644", "$645-675"]
 cutted_capita_categorical_S = pd.cut(per_school_capita_Series,spending_bins, labels=group_names)
+cutted_capita_categorical_S
 
+
+# %%
+# check the bins count
 per_school_capita_Series_grp = per_school_capita_Series.groupby(cutted_capita_categorical_S).count()
+per_school_capita_Series_grp
+#use Seires.value_counts() easiler !!
+cutted_capita_categorical_S.value_counts()
 
-#cutted_capita_categorical_S.value_counts()
-
+# %%
+# add cutted series into same dataframe, then groupby series and count()
 per_school_summary_df['Spending Ranges (Per Student)'] = pd.Series(cutted_capita_categorical_S)
 per_school_summary_df
 
@@ -234,18 +324,20 @@ spending_school_summary_df = pd.DataFrame({"Average Math Score" : spending_math_
 spending_school_summary_df
 
 
-# %% [markdown] 
-# #TABLE No.7: School performance based on the school size
-# ##size_school_summary_df
+# %% [markdown]
+# ## School performance based on the school size
 
 # %%
 size_bins = [0, 1000, 2000, 5000]
 size_bins_labels = ['Small(<1000)','Medium (1000-2000)','Large (2000-5000)']
 cutted_size_categorical_S = pd.cut( per_school_summary_df['Total Students'],size_bins, labels= size_bins_labels)
 per_school_summary_df['School Size Bins'] = pd.Series(cutted_size_categorical_S)
+#alternative version: combine cut and assigned to new column
+# per_school_summary_df["School Size"] = pd.cut(per_school_summary_df["Total Students"], size_bins, labels=size_bins_labels)
 
 per_school_summary_df.head()
-
+# Bins count
+#cutted_size_categorical_S.value_counts()
 
 # %%
 sizeBins_math_scores_Series = per_school_summary_df.groupby(['School Size Bins'])['Average Math Score'].agg('mean')
@@ -261,9 +353,8 @@ size_school_summary_df = pd.DataFrame({"Average Math Score" : sizeBins_math_scor
           "% Overall Passing": sizeBins_overall_passing_percentage_Series.map('{:.0f}'.format)})
 size_school_summary_df
 
-# %% [markdown] 
-# #TABLE No.8: School performance based on the type of school
-# ##type_school_summary_df
+# %% [markdown]
+# ## School performance based on the school type
 # %%
 type_math_scores_Series = per_school_summary_df.groupby(["School Type"]).mean()["Average Math Score"]
 type_reading_scores_Series = per_school_summary_df.groupby(["School Type"]).mean()["Average Reading Score"]
